a53a67072df61d048c4a533942c5775f520c30f2
Ubasic <lmjubuntu@gmail.com>
2014-07-26 19:04:54 +0800

    set the sock name

--- main.go
+++ main.go
@@ -24,8 +24,8 @@ func main() {
     Connect("127.0.0.1:6379")
     sched = NewSched()
     sched.Start()
-    listen, err := net.Listen("unix", "libchan.sock")
-    log.Printf("Started at libchan.sock")
+    listen, err := net.Listen("unix", "huabot-sched.sock")
+    log.Printf("Started at huabot-sched.sock")
     if err != nil {
         log.Fatal(err)
     }
