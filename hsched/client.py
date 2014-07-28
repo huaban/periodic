c2a6556e9fa009507fe9e12489a87eca1842cdc5
Ubasic <lmjubuntu@gmail.com>
2014-07-28 16:31:40 +0800

    use asyncio

--- hsched/client.py
+++ hsched/client.py
@@ -1,6 +1,6 @@
-import socket
 from . import data
 from time import sleep
+import asyncio
 
 def parseHeader(head):
     length = head[0] << 24 | head[1] << 16 | head[2] << 8 | head[3]
@@ -23,16 +23,42 @@ class ConnectionError(Exception):
     pass
 
 
+class BaseClient(object):
+    def __init__(self, reader, writer):
+        self._reader = reader
+        self._writer = writer
+
+
+    @asyncio.coroutine
+    def recive(self):
+        head = yield from self._reader.read(4)
+        length, hasFd = parseHeader(head)
+
+        payload = yield from self._reader.read(length)
+        payload = data.Decode(str(payload, 'utf-8'))
+        return payload
+
+
+    @asyncio.coroutine
+    def send(self, payload):
+        payload = data.Encode(payload)
+        payload = bytes(payload, 'utf-8')
+        header = makeHeader(payload)
+        self._writer.write(header)
+        self._writer.write(payload)
+        yield from self._writer.drain()
+
+
 class Client(object):
     def __init__(self):
-        self.sock = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
+        self._agent = None
         self.connected = False
-        self._sock_file = None
 
 
     def _connect(self):
-        self.sock.connect(self._sock_file)
-        payload = self.recive()
+        reader, writer = yield from asyncio.open_unix_connection(self._sock_file)
+        self._agent = BaseClient(reader, writer)
+        payload = yield from self._agent.recive()
         if payload["type"][0] != "connection":
             raise ConnectionError("error on connection")
 
@@ -46,7 +72,6 @@ class Client(object):
 
 
     def reconnect(self):
-        self.sock.close()
         while True:
             try:
                 connected = self._connect()
@@ -58,16 +83,16 @@ class Client(object):
 
 
     def ping(self):
-        self.send({"cmd": ["ping"]})
-        payload = self.recive()
+        yield from self._agent.send({"cmd": ["ping"]})
+        payload = yield from self._agent.recive()
         if payload.get('message') == 'pong':
             return True
         return False
 
 
     def ask(self):
-        self.send({"cmd": ["ask"]})
-        payload = self.recive()
+        yield from self._agent.send({"cmd": ["ask"]})
+        payload = yield from self._agent.recive()
         if payload.get("msg") == 'no_job' or payload.get("msg") == 'wait_for_job':
                 return None
 
@@ -75,21 +100,4 @@ class Client(object):
 
 
     def done(self, job):
-        self.send({"cmd": ["done"], "job": [job]})
-
-
-    def recive(self):
-        head = self.sock.recv(4)
-        length, hasFd = parseHeader(head)
-
-        payload = self.sock.recv(length)
-        payload = data.Decode(str(payload, 'utf-8'))
-        return payload
-
-
-    def send(self, payload):
-        payload = data.Encode(payload)
-        payload = bytes(payload, 'utf-8')
-        header = makeHeader(payload)
-        self.sock.send(header)
-        self.sock.send(payload)
+        yield from self._agent.send({"cmd": ["done"], "job": [job]})
