f438e6656d297f71c45a734767181199a9e5b2af
Ubasic <lmjubuntu@gmail.com>
2014-08-06 09:45:42 +0800

    First to check the sock_file

--- main.go
+++ main.go
@@ -3,10 +3,12 @@ package main
 import (
     "log"
     "net"
+    "os"
 )
 
 var sched *Sched
 
+const SOCK_FILE = "huabot-sched.sock"
 
 func handleAccept(listen net.Listener) {
     defer listen.Close()
@@ -24,6 +26,16 @@ func main() {
     Connect("127.0.0.1:6379")
     sched = NewSched()
     sched.Start()
+
+    _, err := os.Stat(SOCK_FILE)
+
+    if err == nil || os.IsExist(err) {
+        _, err = net.Dial("unix", SOCK_FILE)
+        if err == nil {
+            panic("Huabot-sched is already started.")
+        }
+        os.Remove(SOCK_FILE)
+    }
     listen, err := net.Listen("unix", "huabot-sched.sock")
     log.Printf("Started at huabot-sched.sock")
     if err != nil {
