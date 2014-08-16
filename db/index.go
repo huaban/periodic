2d58ceb210f56e7b85f9cf3b49946df826a22d44
Ubasic <lmjubuntu@gmail.com>
2014-08-16 10:55:08 +0800

    update db

--- db/index.go
+++ db/index.go
@@ -1,74 +1,7 @@
 package db
 
-import (
-    "github.com/garyburd/redigo/redis"
-    "strconv"
-)
 
 type Index struct {
     Name string
     Score int
-}
-
-func AddIndex(name, member string, score int) (err error) {
-    var key = "index:" + name
-    var conn = pool.Get()
-    defer conn.Close()
-    _, err = conn.Do("ZADD", PREFIX + key, score, member)
-    return
-}
-
-func GetIndex(name, member string) (score int, err error) {
-    var key = "index:" + name
-    var conn = pool.Get()
-    defer conn.Close()
-    score, err = redis.Int(conn.Do("ZSCORE", PREFIX + key, member))
-    return
-}
-
-func RangeIndex(name string, start, stop int, rev ...bool) (retval []Index, err error) {
-    var key = "index:" + name
-    var conn = pool.Get()
-    defer conn.Close()
-    cmd := "ZRANGE"
-    if len(rev) > 0 {
-        if rev[0] {
-            cmd = "ZREVRANGE"
-        }
-    }
-    reply, err := redis.Values(conn.Do(cmd, PREFIX + key, start, stop, "WITHSCORES"))
-    var _key string
-    var score int
-    retval = make([]Index, len(reply)/2)
-    for k, v := range reply {
-        if k % 2 == 1 {
-            score, _ = strconv.Atoi(string(v.([]byte)))
-            retval[(k-1)/2] = Index{_key,score}
-        } else {
-            _key = string(v.([]byte))
-        }
-    }
-    return
-}
-
-func CountIndex(name string) (count int, err error) {
-    var key = "index:" + name
-    var conn = pool.Get()
-    defer conn.Close()
-    count, err = redis.Int(conn.Do("ZCARD", PREFIX + key))
-    return
-}
-
-func DropIndex(name string) (err error) {
-    var key = "index:" + name
-    err = DelObject(key)
-    return
-}
-
-func DelIndex(name, member string) (err error) {
-    var key = "index:" + name
-    var conn = pool.Get()
-    defer conn.Close()
-    _, err = conn.Do("ZREM", PREFIX + key, member)
-    return
 }
--- db/utils.go
+++ db/utils.go
@@ -1,51 +1,10 @@
 package db
 
+
 import (
-    "github.com/garyburd/redigo/redis"
-    "encoding/json"
     "reflect"
     "strings"
 )
-
-const PREFIX = "huabot-sched:"
-
-func GetObject(key string, obj interface{}) (err error) {
-    var data []byte
-    var conn = pool.Get()
-    defer conn.Close()
-    data, err = redis.Bytes(conn.Do("GET", PREFIX + key))
-    if err != nil {
-        return
-    }
-    err = json.Unmarshal(data, obj)
-    return
-}
-
-func SetObject(key string, obj interface{}) (err error) {
-    var data []byte
-    data, err = json.Marshal(obj)
-    if err != nil {
-        return
-    }
-    var conn = pool.Get()
-    defer conn.Close()
-    _, err = conn.Do("SET", PREFIX + key, data)
-    return
-}
-
-func DelObject(key string) (err error) {
-    var conn = pool.Get()
-    defer conn.Close()
-    _, err = conn.Do("DEL", PREFIX + key)
-    return
-}
-
-func NextSequence(name string) (val int, err error) {
-    var conn = pool.Get()
-    defer conn.Close()
-    val, err = redis.Int(conn.Do("INCRBY", PREFIX + "sequence:" + name, 1))
-    return
-}
 
 func GetTableName(obj interface{}) string {
     typeof := reflect.TypeOf(obj)
