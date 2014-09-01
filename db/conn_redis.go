a39e3b607191f89ef6fabceee62695244560b3ae
Ubasic <lmjubuntu@gmail.com>
2014-09-01 16:47:15 +0800

    update command line

--- db/conn_redis.go
+++ db/conn_redis.go
@@ -1,14 +1,16 @@
 package db
 
 import (
+    "strings"
     "github.com/garyburd/redigo/redis"
 )
 
 var pool *redis.Pool
 
 func Connect(server string) {
+    parts := strings.SplitN(server, "://", 2)
     pool = redis.NewPool(func() (conn redis.Conn, err error) {
-        conn, err = redis.Dial("tcp", server)
+        conn, err = redis.Dial("tcp", parts[1])
         return
     }, 3)
 }
--- main_redis.go
+++ main_redis.go
@@ -1,27 +1,45 @@
 package main
 
 import (
-    "flag"
+    "os"
     "huabot-sched/db"
     "huabot-sched/store"
     sch "huabot-sched/sched"
+    "github.com/codegangsta/cli"
 )
-
-var sched *sch.Sched
-
-var redisPort string
-var entryPoint string
-
-
-func init() {
-    flag.StringVar(&entryPoint, "H", "unix://huabot-sched.sock", "host eg: tcp://127.0.0.1:5000")
-    flag.StringVar(&redisPort, "redis", "127.0.0.1:6379", "redis server")
-    flag.Parse()
-}
 
 
 func main() {
-    db.Connect(redisPort)
-    sched = sch.NewSched(entryPoint, store.RedisStorer{})
-    sched.Serve()
+    app := cli.NewApp()
+    app.Name = "huabot-sched"
+    app.Usage = ""
+    app.Flags = []cli.Flag {
+        cli.StringFlag{
+            Name: "H",
+            Value: "unix://huabot-sched.sock",
+            Usage: "the server address eg: tcp://127.0.0.1:5000",
+            EnvVar: "HUABOT_SCHED_PORT",
+        },
+        cli.StringFlag{
+            Name: "redis",
+            Value: "tcp://127.0.0.1:6379",
+            Usage: "the redis server address",
+            EnvVar: "REDIS_PORT",
+        },
+        cli.BoolFlag{
+            Name: "d",
+            Usage: "Enable daemon mode",
+        },
+    }
+    app.Action = func(c *cli.Context) {
+        if c.Bool("d") {
+            db.Connect(c.String("redis"))
+            sched := sch.NewSched(c.String("H"), store.RedisStorer{})
+            sched.Serve()
+        } else {
+            cli.ShowAppHelp(c)
+        }
+    }
+
+    app.Run(os.Args)
 }
