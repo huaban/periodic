1320080dbff01c85ec0e54291bbe76e717a86382
Ubasic <lmjubuntu@gmail.com>
2014-09-02 10:59:26 +0800

    replace panic to log.Fatal

--- cmd/status.go
+++ cmd/status.go
@@ -6,32 +6,33 @@ import (
     "encoding/json"
     "huabot-sched/sched"
     "fmt"
+    "log"
 )
 
 func ShowStatus(entryPoint string) {
     parts := strings.SplitN(entryPoint, "://", 2)
     c, err := net.Dial(parts[0], parts[1])
     if err != nil {
-        panic(err)
+        log.Fatal(err)
     }
     conn := sched.Conn{Conn: c}
     defer conn.Close()
     err = conn.Send(sched.PackCmd(sched.TYPE_CLIENT))
     if err != nil {
-        panic(err)
+        log.Fatal(err)
     }
     err = conn.Send(sched.PackCmd(sched.STATUS))
     if err != nil {
-        panic(err)
+        log.Fatal(err)
     }
     payload, err := conn.Receive()
     if err != nil {
-        panic(err)
+        log.Fatal(err)
     }
     stats := make(map[string]sched.FuncStat)
     err = json.Unmarshal(payload, &stats)
     if err != nil {
-        panic(err)
+        log.Fatal(err)
     }
     for Func, stat := range stats {
         fmt.Printf("Func: %s\tWorker: %d\tJob: %d\tProcessing: %d\n", Func, stat.Worker, stat.Job, stat.Processing)
