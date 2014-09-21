b37a717f8699c1f94a0efabf31afad8fcdb3447e
Ubasic <lmjubuntu@gmail.com>
2014-09-21 16:06:34 +0800

    set version 0.1.1

--- main.go
+++ main.go
@@ -17,7 +17,7 @@ func main() {
     app := cli.NewApp()
     app.Name = "periodic"
     app.Usage = "Periodic task system"
-    app.Version = "0.0.1"
+    app.Version = "0.1.1"
     app.Flags = []cli.Flag {
         cli.StringFlag{
             Name: "H",
