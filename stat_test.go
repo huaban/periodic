63a97c8d7869e018c73fc3f7117c835a53008fe5
Ubasic <lmjubuntu@gmail.com>
2014-11-01 01:11:23 +0800

    update funcstat log description

--- stat_test.go
+++ stat_test.go
@@ -34,6 +34,6 @@ func TestFuncStat(t *testing.T) {
     var stat = NewFuncStat("test")
     stat.Worker.Incr()
     if stat.String() != "test,1,0,0" {
-        t.Fatalf("Func: except: 1,0,0, got: %s\n", stat)
+        t.Fatalf("FuncStat: except: test,1,0,0, got: %s\n", stat)
     }
 }
